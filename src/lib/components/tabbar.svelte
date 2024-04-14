e3dca6b33b9154bf657c4671432d31cfc54698b1
James Hughes <james@jxhug.me>
2024-04-14 12:17:54 -0700

    stuff

---
+++ src/lib/components/tabbar.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+
+
+<div class="h-[75px] w-full bg-[#FA0AFA] flex flex-row c">
+    <p>T</p>
+    <p>2</p>
+</div>
+
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,20 @@
 <script>
     import "../app.css";
-    import Sidebar from '$lib/components/sidebar.svelte'
+
     import { ModeWatcher } from 'mode-watcher';
+    import Tabbar from "$lib/components/tabbar.svelte";
+
+
 </script>
 
 
 <ModeWatcher />
 
-<div class="flex flex-row">
-    <Sidebar />
-
-    <slot />
+<div class="flex flex-col flex-1 min-h-screen">
+    <div class="flex-grow flex overflow-scroll">
+        <slot />        
+    </div>
+    <div class="h-full">
+        <Tabbar />
+    </div>
 </div>
