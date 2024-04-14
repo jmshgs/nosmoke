84dd89d0660f315c24b8c5a5a881974e13c6b55f
James Hughes <james@jxhug.me>
2024-04-14 11:18:09 -0700

    sveltekit byebye tauri

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import "../app.css";
+    import Sidebar from '$lib/components/sidebar.svelte'
+    import { ModeWatcher } from 'mode-watcher';
+</script>
+
+
+<ModeWatcher />
+
+<div class="flex flex-row">
+    <Sidebar />
+
+    <slot />
+</div>
