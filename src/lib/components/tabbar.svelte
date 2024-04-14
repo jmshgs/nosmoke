d84acf4ae3c51782557633546a72f86d2cd3cc21
James Hughes <james@jxhug.me>
2024-04-14 14:05:26 -0700

    sutff

--- src/lib/components/tabbar.svelte
+++ src/lib/components/tabbar.svelte
@@ -1,12 +1,29 @@
 <script>
+    import TabItem from "$lib/components/tabitem.svelte";
+    import Icon from '@iconify/svelte';
+
+    import * as Tabs from "$lib/components/ui/tabs/index.js";
+
+    export let selectedItem = 0;
+
+
+    const CSS_TAB_SELECTED = "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2";
 
 </script>
 
 
 
-<div class="h-[75px] w-full bg-[#FA0AFA] flex flex-row c">
-    <p>T</p>
-    <p>2</p>
+<div class="h-[115px] w-full  flex flex-row items-center justify-center">
+    <div class="h-[70px] bg-secondary rounded-3xl flex flex-row w-[95vw] justify-between items-center px-5 ">
+        <TabItem selected={selectedItem == 0} onClick={() => {selectedItem = 0}}>
+            <Icon icon="mingcute:home-3-fill" width=28 height=28 class="fill-none"/>
+        </TabItem>
+
+        <TabItem selected={selectedItem == 1} onClick={() => {selectedItem = 1}}>
+            <Icon icon="mynaui:chart-graph" width=28 height=28/>
+        </TabItem>    
+
+    </div>
 </div>
 
 
--- src/lib/components/tabitem.svelte
+++ src/lib/components/tabitem.svelte
@@ -1,3 +1,22 @@
 <script>
-    
+
+    import { goto } from '$app/navigation';
+    import { page } from '$app/stores';  
+
+    export let path = "";
+    export let selected = false;
+
+
+
+
 </script>
+
+
+
+
+
+<div class="">
+    <button on:click={onClick} class="w-40 h-12 activated:animate-pulse flex items-center justify-center bg-opacity-15 rounded-xl transition-colors ease-in-out" class:bg-slate-700={selected}>
+        <slot/>
+    </button>
+</div>
